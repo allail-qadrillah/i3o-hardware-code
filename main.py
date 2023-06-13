8c442b06872e3c274bce1aee27bf586b53f5c940
allail-qadrillah <mallailqadrillah43@gmail.com>
2023-06-14 05:32:11 +0700

    push

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import random
 from firebase.api import RTDB
 from time import sleep
 
+
 db = RTDB()
 
 BIAYA_PER_KWH = 1.525
@@ -13,27 +14,26 @@ def biaya_listrik(kwh):
     biaya = kwh * BIAYA_PER_KWH
     return int(round(biaya + ( biaya * 0.1), 3) * 1000)
 
-db.update('/', { 'token': 5000})
+db.update('/', { 'harga_token': 5000})
 
 while True:
 
-    sisa_token = db.read('/token')
+    sisa_token = db.read('/harga_token')
     if sisa_token > 0:
         value_kwh  = random_kwh(0, 3, rounded=1)
         pemakaian_listrik = biaya_listrik(value_kwh)
         token_baru = sisa_token - pemakaian_listrik
 
         if token_baru < 0:
-            print("here")
-            db.update('/', { 'token': 0})
+            db.update('/', { 'harga_token': 0})
             continue
         
         # update sisa token
         else:
-            db.update('/', { 'token': token_baru})
+            db.update('/', {'harga_token': token_baru})
 
     else: 
-        db.update('/', { 'token': 0})
+        db.update('/', {'harga_token': 0})
         print("TOKEN HABISS")
 
     sleep(3)
--- test_raspi.py
+++ test_raspi.py
@@ -1,6 +1,21 @@
-from lcd.app import LCD
+from pzem.app import PZEM
+from time import sleep
+from raspberry.app import Pin 
 
-lcd = LCD()
 
-# lcd.display("coba", 1)
-lcd.progress_bar()
+pzem = PZEM()
+
+# relay.write(HIGH)
+while True:
+  data = pzem.read()
+
+  print(
+    "voltage", pzem.get_voltage(),
+    "current", pzem.get_current(),
+    "power", pzem.get_power(),
+    "energy", pzem.get_energy(),
+    "frekuensi", pzem.get_frekuensi(), 
+    "kwh", round(pzem.get_energy_kwh(), 3)
+  )
+
+  sleep(1)
